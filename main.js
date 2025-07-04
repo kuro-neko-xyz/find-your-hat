208b4b954daeece219478e4765f90956c3b3dae6
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-07-04 14:14:19 -0600

    Finish Game

--- main.js
+++ main.js
@@ -6,22 +6,32 @@ const fieldCharacter = "░";
 const pathCharacter = "*";
 
 class Field {
-  constructor(field) {
+  #depth;
+  #width;
+  #field;
+  #playerCoordinates;
+
+  constructor(field, playerCoordinates = { row: 0, col: 0 }) {
     if (!Array.isArray(field) || !field.every((row) => Array.isArray(row))) {
       throw new Error("Field must be a 2D array");
     }
 
-    this.depth = field.length;
-    this.width = field[0].length;
+    this.#depth = field.length;
+    this.#width = field[0].length;
 
-    if (field.some((row) => row.length !== this.width)) {
+    if (field.some((row) => row.length !== this.#width)) {
       throw new Error("All rows in the field must have the same length");
     }
 
-    this.field = field;
+    this.#field = field;
+    this.#playerCoordinates = playerCoordinates;
   }
 
   static generateField(depth, width, holesCount) {
+    if (holesCount > depth * width - 2) {
+      throw new Error("Too many holes for the given field size");
+    }
+
     const matrix = Array.from({ length: depth }, () =>
       Array(width).fill(fieldCharacter)
     );
@@ -37,17 +47,6 @@ class Field {
       }
     }
 
-    let isPlayerSpawned = false;
-    while (!isPlayerSpawned) {
-      const row = Math.floor(Math.random() * depth);
-      const col = Math.floor(Math.random() * width);
-
-      if (matrix[row][col] === fieldCharacter) {
-        matrix[row][col] = pathCharacter;
-        isPlayerSpawned = true;
-      }
-    }
-
     let isHatPlaced = false;
     while (!isHatPlaced) {
       const row = Math.floor(Math.random() * depth);
@@ -59,39 +58,79 @@ class Field {
       }
     }
 
-    const field = new Field(matrix);
+    let isPlayerSpawned = false;
+    let playerCoordinates = { row: 0, col: 0 };
+    while (!isPlayerSpawned) {
+      const row = Math.floor(Math.random() * depth);
+      const col = Math.floor(Math.random() * width);
+
+      if (matrix[row][col] === fieldCharacter) {
+        matrix[row][col] = pathCharacter;
+        isPlayerSpawned = true;
+        playerCoordinates = { row, col };
+      }
+    }
+
+    const field = new Field(matrix, playerCoordinates);
 
     return field;
   }
 
+  #calculateNewPosition(playerCoordinates, key) {
+    const newPosition = { ...playerCoordinates };
+
+    switch (key) {
+      case "w":
+        newPosition.row = Math.max(0, playerCoordinates.row - 1);
+        break;
+      case "s":
+        newPosition.row = Math.min(this.#depth - 1, playerCoordinates.row + 1);
+        break;
+      case "a":
+        newPosition.col = Math.max(0, playerCoordinates.col - 1);
+        break;
+      case "d":
+        newPosition.col = Math.min(this.#width - 1, playerCoordinates.col + 1);
+        break;
+      default:
+        console.log("Invalid move. Use w/a/s/d to move.");
+        return playerCoordinates;
+    }
+
+    return newPosition;
+  }
+
   print() {
-    for (let row of this.field) {
+    for (let row of this.#field) {
       console.log(row.join(""));
+    }
+  }
+
+  handleMove(key) {
+    const newPosition = this.#calculateNewPosition(
+      this.#playerCoordinates,
+      key
+    );
+
+    switch (this.#field[newPosition.row][newPosition.col]) {
+      case hole:
+        console.log("You fell into a hole! Game over.");
+        process.exit();
+      case hat:
+        console.log("Congratulations! You found the hat!");
+        process.exit();
+      default:
+        this.#field[newPosition.row][newPosition.col] = pathCharacter;
+        this.#playerCoordinates = newPosition;
     }
   }
 }
 
-const myField = Field.generateField(5, 5, 3);
+const myField = Field.generateField(10, 10, 15);
 
 while (true) {
+  console.clear();
   myField.print();
   const input = prompt("Enter your move (w/a/s/d): ").toLowerCase();
-
-  switch (input) {
-    case "w":
-      console.log("You moved up.");
-      break;
-    case "a":
-      console.log("You moved left.");
-      break;
-    case "s":
-      console.log("You moved down.");
-      break;
-    case "d":
-      console.log("You moved right.");
-      break;
-    default:
-      console.log("Invalid input. Please use w/a/s/d to move.");
-      break;
-  }
+  myField.handleMove(input);
 }
